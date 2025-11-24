6eaea4b4ed6ac690bffdcab472082f27866ba8ff
paulatte28 <kcoretico@gmail.com>
2025-11-24 11:00:08 +0800

    idk

--- lib/login.dart
+++ lib/login.dart
@@ -19,7 +19,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
+      appBar: AppBar(title: Text('Login - Coretico')),
         body: Center(
           child: SingleChildScrollView(
             padding: EdgeInsets.all(16),
